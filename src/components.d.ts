0c92b093e69c93d26034ba5ba947b2ef96ce9590
Jonas dev <jonas@amps.one>
2024-05-06 20:45:42 +0000

    start playing with apu

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    AppFooter: typeof import('./components/AppFooter.vue')['default']
+    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
+    PlaylistTest: typeof import('./components/PlaylistTest.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
---
+++ src/components/PlaylistTest.vue
@@ -0,0 +1,10 @@
+<template>
+  asd
+</template>
+
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import {PlaylistService} from "@/ta_openapi"
+</script>
